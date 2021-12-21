85eff976f0d78f83f29d595180b7547ae3c16d74
Lyamir <xlatiax@gmail.com>
2021-12-22 02:42:17 +0800

    added Item functions

--- model/item.js
+++ model/item.js
@@ -5,4 +5,51 @@ const Item = function (title){
   this.completed = false
 }
 
+Item.add = (newItem, result) => {
+  db.query("INSERT INTO items SET ?", [newItem.title], (err, res) => {
+    if (err){
+      console.log("error: ", err)
+      result(err, null)
+      return
+    }
+
+    result(null, {id: res.insertID, ...newItem})
+  })
+}
+
+Item.getAll = result => {
+  db.query("SELECT * FROM items", (err, res) => {
+    if (err){
+      console.log("error:", err)
+      result(null, err)
+      return
+    }
+
+    result(null, res)
+  })
+}
+
+Item.delete = (id, result) => {
+  db.query("DELETE FROM items WHERE id = ?", [id], (err, res) => {
+    if (err){
+      console.log("error:", err)
+      result(null, err)
+      return
+    }
+
+    result(null, res)
+  })
+}
+
+Item.changeStatus = (item, result) => {
+  db.query("UPDATE items SET complete = ? WHERE id = ?", [!item.complete, item.id], (err, res) => {
+    if (err){
+      console.log("error: ", err)
+      result(err, null)
+      return
+    }
+    result(null, `Updated post with ID: ${item.id}`)
+  })
+}
+
 module.exports = Item
