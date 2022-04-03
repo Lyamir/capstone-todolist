3b1547c215c5d5690871d9b4cdbe11bf41563dc4
Lyamir <xlatiax@gmail.com>
2022-04-04 00:10:45 +0800

    added router functions

--- controller/indexController.js
+++ controller/indexController.js
@@ -1,7 +1,88 @@
 const Item = require('../model/item')
 
 const routerFunctions = {
-  
+  getItems: (req, res) => {
+    Item.getAll((err, data) => {
+      if(err)
+        res.sendStatus(500).send({
+          message: err.message || "Error"
+        })
+      else
+        res.render('index', {
+          items: data
+        })
+    })
+  },
+
+  addItem: (req, res) => {
+    if (!req.body)
+      res.sendStatus(500).send({
+        message: "Content cannot be empty"
+      })
+    
+    const item = {
+      title: req.body.title,
+      dueDate: req.body.dueDate
+    }
+
+    Item.add(item, (err, data) => {
+      if (err)
+        res.sendStatus(500).send({
+          message: err.message || "Error"
+        })
+      else res.redirect('/')
+    })
+  },
+
+  deleteItem: (req, res) => {
+    if (!req.body)
+      res.sendStatus(500).send({
+        message: "Content cannot be empty"
+      })
+    
+    Item.delete(req.params.id, (err, data) => {
+      if (err)
+        res.sendStatus(500).send({
+          message: err.message || "Error"
+        })
+      else res.redirect('/')
+    })
+  },
+
+  updateItem: (req, res) => {
+    if(!req.body)
+      res.sendStatus(500).send({
+        message: "Content cannot be empty"
+      })
+
+      const item = {
+        id: req.params.id,
+        complete: parseInt(req.body.complete)
+      }
+
+      Item.changeStatus(item, (err,data) => {
+        if (err)
+          res.sendStatus(500).send({
+            message: err.message || "Error"
+          })
+        else res.redirect('/')
+      })
+  },
+
+  completeAllItems: (req, res) => {
+    if(!req.body)
+      res.sendStatus(500).send({
+        message: "Content cannot be empty"
+      })
+    
+    Item.completeAll((err, data) => {
+      if (err)
+      res.sendStatus(500).send({
+        message: err.message || "Error"
+      })
+      else res.redirect('/')
+    })
+  }
 }
 
 module.exports = routerFunctions
