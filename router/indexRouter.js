5d99820ae871b9e03e3b9b38fb2d4f8da39d7d6e
Lyamir <xlatiax@gmail.com>
2022-04-04 00:11:54 +0800

    added routes

--- router/indexRouter.js
+++ router/indexRouter.js
@@ -3,4 +3,10 @@ const app = express()
 const router = express.Router()
 const controller = require('../controller/indexController')
 
+router.get('/', controller.getItems)
+router.post('/', controller.addItem)
+router.get('/delete/:id', controller.deleteItem)
+router.post("/update/:id", controller.updateItem)
+router.get("/update", controller.completeAllItems)
+
 module.exports = router
