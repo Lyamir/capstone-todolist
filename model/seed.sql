f67e75b8450e842543c39ba38c8a77746a7e72a2
Lyamir <xlatiax@gmail.com>
2022-04-04 00:11:23 +0800

    updated items table

--- model/seed.sql
+++ model/seed.sql
@@ -4,6 +4,7 @@ USE capstone_todolist;
 CREATE TABLE IF NOT EXISTS `items` (
   `id` int NOT NULL AUTO_INCREMENT,
   `title` text,
+  `dueDate` date,
   `complete` boolean,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`) 
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
