3c823237c7ab5e516d0ef0aba382b16f1bcf036a
Developer <developer@development.com>
2022-04-13 01:43:48 +0800

    changed date input type to datetime. Added testing

--- test/test.js
+++ test/test.js
@@ -1,4 +1,5 @@
-const {Builder, By, Key, ulit, WebDriver, Webelement} = require ("selenium-webdriver");
+const { List } = require("mocha/lib/reporters");
+const {Builder, By, Key, ulit, WebDriver, Webelement, WebElement} = require ("selenium-webdriver");
 const firefox = require("selenium-webdriver/firefox");
 const should = require("chai").should();
 
@@ -11,6 +12,12 @@ function makeid(length) {
  charactersLength));
     }
     return result;
+}
+
+let item = {
+    name:makeid(10),
+    date:"01012023",
+    time:"1200AM"
 }
 
 let address = "http://localhost:3000" //to be changed
@@ -34,15 +41,31 @@ describe("Testing Connection", async function(){
     });
 });
 
-describe("Unit Tests", async function(){
+describe.only("Unit Tests", async function(){
     before(function(){
         connected.should.be.true;
     });
-    it("first test", async function(){
+    it("It should add an item", async function(){
         let options = new firefox.Options();
         options.addArguments("-headless");
         let driver = await new Builder().forBrowser("firefox").setFirefoxOptions(options).build();
         await driver.get(address);
+
+        console.log("name "+item.name);
+        console.log("date "+item.date);
+
+        await driver.findElement(By.id("title")).sendKeys(item.name);
+        let dateInput = await driver.findElement(By.id("dueDate"))
+        await dateInput.click();
+        await dateInput.sendKeys(item.date, Key.TAB, item.time);
+
+        await driver.findElement(By.xpath("/html/body/form[1]/button")).click();
+
+        await driver.get(address);
+
+        let testName = await driver.findElements(By.xpath("//*[contains(text(), \""+item.name+"\")]"));
+
+        testName.should.not.be.empty;
 
         await driver.quit();
     });
